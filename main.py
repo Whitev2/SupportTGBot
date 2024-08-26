619dd5c22ddba1e27d2750c0b81b299554b94243
Whitev2 <WilliamHarris9584680@gmail.com>
2024-08-26 20:10:14 +0300

    init

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from aiogram.types import Message, CallbackQuery
 
 
 API_TOKEN = '7516642839:AAGdJf-My3DAvOt9Ab_mcoKDou6w-ERpWIg'
-SUPPORT_CHAT_ID = '-4552814838'
+SUPPORT_CHAT_ID = '-1002188332138,'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,13 +24,13 @@ dp = Dispatcher(storage=storage)
 class ConfirmPaymentCallback(CallbackData, prefix="confirm"):
     user_id: int
     message_id: int  # Adding field to store message_id
+    data: str
 
 
 # Handling the /start command
 @dp.message(Command("start"))
 async def start_handler(message: Message):
     await message.answer("Hello! Please send a photo with payment confirmation.")
-
 
 # Handling the receipt of a photo from the user
 @dp.message(lambda message: message.photo)
@@ -39,14 +39,15 @@ async def handle_payment_photo(message: Message):
     photo = message.photo[-1]  # Take the photo in maximum quality
 
     keyboard = InlineKeyboardBuilder()
-    callback_data = ConfirmPaymentCallback(user_id=message.from_user.id, message_id=message.message_id)
-    keyboard.button(text="Confirm", callback_data=callback_data.pack())
+    keyboard.button(text="Confirm", callback_data=ConfirmPaymentCallback(user_id=message.from_user.id, message_id=message.message_id, data="Confirm").pack())
+    keyboard.button(text="Failed", callback_data=ConfirmPaymentCallback(user_id=message.from_user.id, message_id=message.message_id, data="Failed").pack())
+    keyboard.button(text="Waiting for bank report", callback_data=ConfirmPaymentCallback(user_id=message.from_user.id, message_id=message.message_id, data="Waiting").pack())
 
-    await bot.send_photo(SUPPORT_CHAT_ID, photo.file_id,
+    await bot.send_photo(chat_id=SUPPORT_CHAT_ID, photo=photo.file_id,
                          caption=f"User @{message.from_user.username} sent a payment confirmation.",
                          reply_markup=keyboard.as_markup())
 
-    await message.answer("The photo has been sent for review. Please wait for confirmation.")
+    await message.answer("This transaction has been sent to review. Please wait for confirmation. ")
 
 
 # Handling the pressing of the "Confirm" button
@@ -55,11 +56,17 @@ async def confirm_payment(callback_query: CallbackQuery, callback_data: ConfirmP
     # Extracting user_id and message_id from callback_data
     user_id = callback_data.user_id
     message_id = callback_data.message_id
+    data = callback_data.data
 
     # Sending confirmation message to the user
     try:
         # Replying to the user's photo message
-        await bot.send_message(chat_id=user_id, text="Your transaction has been confirmed!", reply_to_message_id=message_id)
+        if data == "Confirm":
+            await bot.send_message(chat_id=user_id, text="Your transaction has been confirmed!", reply_to_message_id=message_id)
+        elif data == "Failed":
+            await bot.send_message(chat_id=user_id, text="Your transaction has been failed!", reply_to_message_id=message_id)
+        elif data == "Waiting":
+            await bot.send_message(chat_id=user_id, text="Your transaction waiting for bank report!", reply_to_message_id=message_id)
 
         # Removing the button after confirmation
         await bot.edit_message_reply_markup(
